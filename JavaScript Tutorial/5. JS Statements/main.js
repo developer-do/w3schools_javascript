f68aeef687d9b50cdc36b6d7b6c9424763debd6d
developer-do <dev-do@naver.com>
2018-12-18 23:11:00 +0900

    5. JS Statements

---
+++ JavaScript Tutorial/5. JS Statements/main.js
@@ -0,0 +1,6 @@
+demo1 = document.getElementById("demo1");
+demoBtn1 = document.getElementById("demoBtn1");
+
+demoBtn1.addEventListener("click", function () {
+  demo1.innerHTML = "Hello Dolly";
+});
