45df62eec3ca8bfaf607f3387ebf41ec9377e17e
developer-do <dev-do@naver.com>
2018-12-19 02:05:37 +0900

    8. JS Variables

---
+++ JavaScript Tutorial/8. JS Variables/main.js
@@ -0,0 +1,11 @@
+/* 처음 예제 */
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log("z = " + z);
+
+/* 대수와 비슷합니다. */
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+console.log("total = " + total);
