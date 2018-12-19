c6deaa1825c9368f4ad73641b22b79d8d308070f
developer-do <dev-do@naver.com>
2018-12-19 17:18:30 +0900

    16. JS Strings

---
+++ JavaScript Tutorial/16. JS Strings/main.js
@@ -0,0 +1,25 @@
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+console.log(sln);
+
+var x = "We are the so-called \"Vikings\" from the north";
+console.log(x);
+var x = 'It\'s alright';
+console.log(x);
+var x = "The character \\ is called backslash.";
+console.log(x);
+var x = "모르겟다야~";
+console.log(x);
+
+var x1 = "Ponny";
+var y1 = new String("Ponny");
+
+console.log("x1 == y1 => " + (x1 == y1));
+console.log("x1 === y1 => " + (x1 === y1));
+
+var x2 = new String("popo");
+var y2 = new String("popo");
+
+console.log("x2 == y2 => " + (x2 == y2));
+console.log("x2 === y2 => " + (x2 === y2));
