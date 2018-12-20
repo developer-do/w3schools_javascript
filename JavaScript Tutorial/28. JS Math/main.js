9ab78c24e64cde7670e593f5ded6270ae036fe26
developer-do <dev-do@naver.com>
2018-12-20 22:41:00 +0900

    28. JS Math

---
+++ JavaScript Tutorial/28. JS Math/main.js
@@ -0,0 +1,33 @@
+console.log("Math.PI =",Math.PI);
+console.log("Math.round(4.7) =", Math.round(4.7));
+console.log("Math.round(4.4) =", Math.round(4.4));
+console.log("Math.pow(8, 2) =", Math.pow(8, 2));
+console.log("Math.sqrt(64) =", Math.sqrt(64));
+console.log("Math.abs(-4.7) =", Math.abs(-4.7));
+console.log("Meth.ceil(4.4) =", Math.ceil(4.4));
+console.log("Math.floor(4.7) =", Math.floor(4.7));
+console.log("Math.sin(90 * Math.PI / 180) =", Math.sin(90 * Math.PI / 180));
+console.log("Math.cos(0 * Math.PI / 180) =", Math.cos(0 * Math.PI / 180));
+console.log("Math.min(0, 150, 30, 20, -8, -200) =",Math.min(0, 150, 30, 20, -8, -200));
+console.log("Math.max(0, 150, 30, 20, -8, -200) =", Math.max(0, 150, 30, 20, -8, -200));
+console.log("Math.random() =", Math.random());
+console.log("Math.E       =", Math.E       );
+console.log("Math.PI      =", Math.PI      );
+console.log("Math.SQRT2   =", Math.SQRT2   );
+console.log("Math.SQRT1_2 =", Math.SQRT1_2 );
+console.log("Math.LN2     =", Math.LN2     );
+console.log("Math.LN10    =", Math.LN10    );
+console.log("Math.LOG2E   =", Math.LOG2E   );
+console.log("Math.LOG10E  =", Math.LOG10E  );
+
+
+
+
+
+
+
+
+
+
+
+
