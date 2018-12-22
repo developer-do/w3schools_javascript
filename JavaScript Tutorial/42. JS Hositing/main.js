9af9475d7dcc0faf6ad3da8575449f16771498fa
dev-do <dev-do@naver.com>
2018-12-22 17:52:52 +0900

    43. JS Use Strict

---
+++ JavaScript Tutorial/42. JS Hositing/main.js
@@ -0,0 +1,35 @@
+x = 5;  // Assign 5 to x
+
+elem = document.getElementById("demo");   // Find an element
+elem.innerHTML = x;                       // Display x in the element
+
+var x;  // Declare x
+
+var x1;  // Declare x
+x1 = 5;  // Assign 5 to x
+
+elem = document.getElementById("demo1"); // Find an element
+elem.innerHTML = x1;                   // Display x in the element
+
+/* */
+
+var x = 5;  // Initialize x
+var y = 7;  // Initialize y
+
+elem = document.getElementById("demo2");  // Find an element
+elem.innerHTML = x + " " + y;             // Display x and y
+
+var x1 = 5;  // Initialize x
+
+elem = document.getElementById("demo3");  // Find an element
+elem.innerHTML = x1 + " " + y1;             // Display x and y
+
+var y1 = 7;  // Initialize y
+
+var x2 = 5;  // Initialize x
+var y2;      // Declare y
+
+elem = document.getElementById("demo4");  // Find an element
+elem.innerHTML = x2 + " " + y2;
+
+y2 = 7;      // Assign 7 to y
