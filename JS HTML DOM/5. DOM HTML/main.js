d6ffb3c1726e5efe5f596f55fdc51750d0e65188
developer-do <dev-do@naver.com>
2018-12-26 15:41:30 +0900

    5. DOM HTML

---
+++ JS HTML DOM/5. DOM HTML/main.js
@@ -0,0 +1,9 @@
+document.getElementById("dateTime").innerHTML = Date();
+document.getElementById("p1").innerHTML = "New text!!";
+
+var element = document.getElementById("id01");
+element.innerHTML = "New Heading";
+
+function changeFunc() {
+  document.getElementById("myImage").src = "landscape.jpg";
+}
