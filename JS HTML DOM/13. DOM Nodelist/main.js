80b405a82794e5fcccf1b00f3de45e98445feabc
dev-do <dev-do@naver.com>
2018-12-28 15:16:09 +0900

    13. DOM Nodelist

---
+++ JS HTML DOM/13. DOM Nodelist/main.js
@@ -0,0 +1,7 @@
+var elemP = document.querySelectorAll("p");
+var i;
+for(i = 0; i < elemP.length; i++) {
+  elemP[i].style.background = "lightblue";
+  elemP[i].style.color = "red";
+  elemP[i].style.fontWeight = "bold";
+}
