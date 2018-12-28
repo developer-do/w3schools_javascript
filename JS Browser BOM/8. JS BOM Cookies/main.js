36fa044224cc260162f357877cbc59232cfcda20
developer-do <dev-do@naver.com>
2018-12-29 01:32:14 +0900

    8. JS BOM Cookies

---
+++ JS Browser BOM/8. JS BOM Cookies/main.js
@@ -0,0 +1,41 @@
+
+
+document.cookie = "username=John Smith; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+var x = document.cookie;
+console.log(x);
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while(c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if(c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+function checkCookie() {
+  var username = getCookie("username");
+  if(username != "") {
+    alert("Welcom again " + username);
+  } else {
+    username = prompt("Please enter your name:", "");
+    if(username != "" && username != null) {
+      setCookie("username", username, 365);
+    }
+  }
+}
