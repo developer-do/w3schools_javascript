3c4ea45e8c646e742c440523806460d5588cc94f
dev-do <dev-do@naver.com>
2018-12-28 16:16:08 +0900

    2. JS BOM Screen

---
+++ JS Browser BOM/2. JS BOM Screen/main.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "Screen Width: " + screen.width + "px";
+document.getElementById("demo1").innerHTML = "Screnn Height: " + screen.height + "px";
+document.getElementById("demo2").innerHTML = "Available Screen Width : " + screen.availWidth + "px";
+document.getElementById("demo3").innerHTML = "Available Screen Height: " + screen.availHeight + "px";
+document.getElementById("demo4").innerHTML = "Screen Color Depth: " + screen.colorDepth;
+document.getElementById("demo5").innerHTML = "Screen Pixel Depth: " + screen.pixelDepth;
